35f01ac895c0c243e811d76ee6c1bdddbba3c0e5
ah1ahwon <100198970+ah1ahwon@users.noreply.github.com>
2023-02-07 19:56:14 +0900

    도로명 주소 확인
    고객이 수기로 작성하여 올바른 주소인지 보장할 수 없는 대량의 주소 목록이 있습니다.
    건 별로 주소를 검사하여, 주소가 어느 "로"("길")인지 확인하려고 합니다.

---
+++ JeongAhwon.java/JeongAhwon.zip_expanded/JeongAhwon.java/src/App.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.util.regex.*;
+
+public class App {
+    @SuppressWarnings("resource")
+	public static void main(String[] args) throws Exception {  
+    	Regex regex = new Regex(); 
+    	System.out.println("종료 : Q\n");
+    	while (true) {
+        	System.out.println("주소를 입력해주세요.");
+        	System.out.print("입력 : ");
+        	Scanner sc = new Scanner(System.in);
+            String add = sc.nextLine();
+            if(add.equals("Q") ||  add.equals("q")) {
+            	break;
+            }
+            System.out.printf("출력 : %s\n\n",  regex.getAddress(add));
+    	}
+    	System.out.println("\n-종료-");
+    	return;
+    }
+}
+
+class Regex{
+     String getAddress(String add){
+       String gu = "";
+       String doro = "";
+       
+       String address = add.replaceAll(" ", "");
+       final String GU_PATTERN = "(([가-힣A-Za-z·\\d~\\-\\.]{2,}(시|군|구)+))";
+       Matcher getGu = Pattern.compile(GU_PATTERN).matcher(address);
+       while (getGu.find()) {
+    	   if(getGu.group()!=null) {
+    		   gu = getGu.group().replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9]", "");
+    		   break;
+    	   }
+    	}
+
+       
+       add = add.replaceAll(" ", "");
+       if(gu != "") {
+    	   add = add.split(GU_PATTERN)[1];
+       }
+       
+       final String DORO_PATTERN = "(([가-힣A-Za-z·\\d~\\-\\.]{2,}(로|길)+))";
+       Matcher getDoro = Pattern.compile(DORO_PATTERN).matcher(add);
+       while (getDoro.find()) {
+    	   if(getDoro.group(0)!=null) {
+    		   doro = getDoro.group(0).replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9]", "");
+    		   break;
+		   }
+	   }
+       return (gu + ' ' + doro).trim();
+    }
+     
+     
+}
